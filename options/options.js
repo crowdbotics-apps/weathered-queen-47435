abde6e94cd65e2d26496182b380e85163ba4101d
Crowdbotics <team@crowdbotics.com>
2024-04-09 21:48:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-queen-47435",
+  url: "https://weathered-queen-47435.botics.co",
+  api: "https://weathered-queen-47435.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
